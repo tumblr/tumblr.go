debadc0f1bf73ebd63b9ad74b324970e06a6ba70
Jason Penny <jpenny@tumblr.com>
2018-06-17 07:59:25 -0400

    gofmt post.go for new fields

--- post.go
+++ post.go
@@ -191,8 +191,8 @@ type VideoPost struct {
 		VideoId string `json:"video_id"`
 	} `json:"video"`
 	VideoType string `json:"video_type"`
-	VideoUrl string `json:"video_url"`
-	Duration uint32 `json:"duration"`
+	VideoUrl  string `json:"video_url"`
+	Duration  uint32 `json:"duration"`
 }
 
 // PhotoPost represents a Photo Post.
